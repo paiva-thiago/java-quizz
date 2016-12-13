ce5cd9810a414bf193fc0c8348dc59407458efdc
Thiago Paiva <Thiago Paiva>
2016-12-12 23:46:38 -0200

    4-Objetos

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 /* O JAVASCRIPT
  * Aqui é nosso palco principal, onde iremos controlar os elementos HTML e aplicar dinâmica e lógica para nossa página.
- * Vamos agora criar uma função para avaliar se nosso jogador respondeu a pergunta corretamente
+ * Neste passo não teremos nada por aqui. Vamos para um cara novo, que está na mesma pasta deste arquivo. É o questionador.js.
  */
 
 function apareca(){
@@ -19,13 +19,15 @@ function clicar(){
     }
 }
 function conferirResposta(){
-    //Cara novo por aqui!
-    //Primeiro precisamos pegar o que o nosso usuário respondeu. Se notar é um input radio, e todos com o mesmo nome.
-    //Basta usarmos a linha abaixo. 
     var respostaDoUsuario = document.forms.pergunta.alternativa.value;
-    /*Ele deverá realizar uma validação, se nada for preenchido, a variável acima estará em branco!
-    
-    SOMENTE SE ESTA VALIDAÇÃO FOR VERDADEIRA, faça os seguintes passos:
-    1) Crie uma variável respostaCorreta, e pegue o atributo value do input hidden que está no HTML
-    2) Compare as duas informações, e informe ao usuário se ele acertou ou errou.*/
+    if(respostaDoUsuario.trim()!=""){
+        var respostaCorreta = document.getElementById("correta").value;
+        if(respostaDoUsuario==respostaCorreta){
+            alert("Resposta Correta!");
+        }else{
+            alert("Resposta INCORRETA!")
+        }
+    }else{
+        alert("Selecione uma alternativa!");
+    }
 }
---
+++ js/questionador.js
@@ -0,0 +1,20 @@
+/*Agora vamos usar mais um arquivo, este para separar as funções responsáveis pela construção das perguntas!
+    Vamos usar vetores e objetos para isto.
+    Objeto armazenará as características das perguntas, e o vetor armazenará um grupo dos objetos. Varreremos este vetor durante o jogo!
+    */
+/*********OBJETOS*********************
+para criar um objeto podemos fazer simplesmente isto
+var obj={}; Este é um objeto vazio.
+Esta estrutura é chamada  de JSON - Java Script Object Notation
+sempre fazemos a seguinte estrutura
+var objeto = {
+    propriedade:valor,
+    outraProp:valor
+}
+Com isto, crie uma função, que tenha 5 argumentos (frase da pergunta, 3 alternativas e o número da alternativa correta).
+Estes argumentos deverão alimentar as propriedades do objeto, que deverá ter o retorno da função.
+*/
+
+
+
+

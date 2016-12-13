af5161ef29b927ee1bb3b4712d4d4a644adeaed8
Thiago Paiva <Thiago Paiva>
2016-12-13 14:42:24 -0200

    10 - Finalizando as perguntas

--- js/js.js
+++ js/js.js
@@ -1,17 +1,11 @@
 /* O JAVASCRIPT
- * Aqui é nosso palco principal, onde iremos controlar os elementos HTML e aplicar dinâmica e lógica para nossa página.
- * Se você testou o jogo, notou que ele não fez absolutamente nada no final do jogo. 
- * Nos browsers mais recentes, temos um recurso através do F12 - é a área do desenvolvedor, podemos depurar nosso código, e acompanhar  
- se algum recurso simplesmente não foi carregado, ou ainda - se houve erro no javascript. Se você testar com este recurso aberto, vai
- ver que algo aparece no console como erro, provavelmente uma mensagem parecida com esta:
-                                        TypeError: pergunta is undefined
- * Ele tentou acessar uma posição inexistente no vetor, e retornou undefined, é um tipo primitivo do javascript que indica que NENHUM 
- valor foi atribuido e se ele não tem valor algum... Ele nem objeto é! Isto aconteceu por tentar acessar um valor além da quantidade de
- vetores.
- 
- Então, lá na função responder, onde é acrescentado 1 ao idx ,vamos verificar se idx é maior que a dimensão do vetor. Acessamos este valor
- através do atributo length. (exemplo: perguntas.length). Se esta comparação for verdadeira, idx deverá voltar a 0. Depois trataremos o 
- final do jogo.
+ * Vamos agora dar um fim a este jogo!
+ * Vamos encerrar este loop de perguntas. Assim como fizemos na função clicar() vamos fazer aqui, mas ocultando a div id jogo  e exibindo
+   a div id acabou (sim, veja no html, tem elemento novo por lá!). Vamos colocar isto em uma função.
+   
+   Nesta função você também alterará o innerHTML do span com id finalJogador! Não estranhe a pontuação zerada, vamos fazer isso no próximo passo!
+   
+   Com a função feita, troque a linha que zera o idx pela chamada da função.
  */
 
 var perguntas = geraQuizz();
@@ -51,6 +45,9 @@ function conferirResposta(){
             alert("Resposta INCORRETA!")
         }
         idx=idx+1;
+        if(idx>=perguntas.length){
+            idx=0;    
+        }        
         atualiza(idx);
     }else{
         alert("Selecione uma alternativa!");

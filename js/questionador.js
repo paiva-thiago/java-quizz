c4f5aab64cba7066ae10dc6fc2936b20d9e96f2e
Thiago Paiva <Thiago Paiva>
2016-12-12 23:52:16 -0200

    5 - Vetores

--- js/questionador.js
+++ js/questionador.js
@@ -2,19 +2,33 @@
     Vamos usar vetores e objetos para isto.
     Objeto armazenará as características das perguntas, e o vetor armazenará um grupo dos objetos. Varreremos este vetor durante o jogo!
     */
-/*********OBJETOS*********************
-para criar um objeto podemos fazer simplesmente isto
-var obj={}; Este é um objeto vazio.
-Esta estrutura é chamada  de JSON - Java Script Object Notation
-sempre fazemos a seguinte estrutura
-var objeto = {
-    propriedade:valor,
-    outraProp:valor
-}
-Com isto, crie uma função, que tenha 5 argumentos (frase da pergunta, 3 alternativas e o número da alternativa correta).
-Estes argumentos deverão alimentar as propriedades do objeto, que deverá ter o retorno da função.
+    function addPergunta(nmPergunta,nmPrimeira,nmSegunda,nmTerceira,idCerta){
+		var questao = {
+				perg		:	nmPergunta,
+				primeira 	:	nmPrimeira,
+				segunda		:	nmSegunda,
+				terceira	:	nmTerceira,
+				certa		:	idCerta
+		};
+		return questao;
+	}
+/****VETORES*********
+
+São grupos de elementos do mesmo tipo, em resumo esta é sua definição.
+
+Podemos criar com um simples 
+var vetor = [];
+Este é um vetor vazio. Temos métodos que permitem adicionar elementos.
+unshift(item) adiciona na posição inicial do meu vetor
+push(item) adiciona na posição final do meu vetor.
+Ambos retornam a nova quantidade de itens.
+
+Podemos também remover itens com métodos
+pop() elimina o último item
+shift() elimina o primeiro
+
+Ambos retornam quem foi eliminado.
+
+Crie uma função que crie um vetor, adicione neste vetor várias perguntas criadas com a função acima. Use a criatividade para as perguntas.
+Esta nova função deverá ter como retorno o próprio vetor.
 */
-
-
-
-
